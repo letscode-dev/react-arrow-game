827943f5eaefca40bb5da51de04141a2eaef5db9
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-09-21 11:31:20 +0400

    starter

--- src/components/Playground/store/__tests__/initialState.spec.ts
+++
@@ -1,9 +0,0 @@
-import playgroundReducer, { initialState } from "../slices"
-
-describe("initial state", () => {
-  it("check initial state", () => {
-    expect(playgroundReducer(undefined, { type: "unknown" })).toEqual(
-      initialState,
-    )
-  })
-})
